83659c9e245251086659c8ae65248a9a93c13ab2
Ankur Kumar <kumarankur0006@gmail.com>
2025-01-18 12:57:26 +0530

    experiment.py modifed

--- Experiment.py
+++ Experiment.py
@@ -2,7 +2,7 @@
 # from src.components.data_transformation import Data_Transformation
 # from src.entity.config_entity import DataIngestionConfig
 # from src.entity.config_entity import FeatureExtractionConfig
-import numpy as np
+# import numpy as np
 
 # dt = Data_Transformation(DataIngestionConfig.data_store_file_path)
 # user_item = dt.User_item()
@@ -15,19 +15,25 @@ import numpy as np
 # data = DataIngestion()
 # data.initiate_data_ingestion()
 
-# from src.utils.mlflow_handler import MLflowHandler
-# tracking_uri = "https://dagshub.com/AnkurK07/Adversarial-Multimedia-Rec-Visual.mlflow"
-# experiment_name = 'Experiment_1'
-# repo_owner='AnkurK07'
-# repo_name='Adversarial-Multimedia-Rec-Visual'
+
 
 # handler = MLflowHandler(
 #     tracking_uri=tracking_uri,
 #     repo_owner=repo_owner,
 #     repo_name=repo_name
 # )
-from src.Adversial_Multimedia_Recommendation.AMR_Model import AMRModel
-from src.entity.config_entity import ModelsaveConfig
-model = AMRModel.load_model(ModelsaveConfig.model_store_file_path)
-x = model.predict(None,('AFGDRVPCP742YM5MMLFIKZCGNNRQ',6))
-print(x)
+# from src.Adversial_Multimedia_Recommendation.AMR_Model import AMRModel
+# from src.entity.config_entity import ModelsaveConfig
+# model = AMRModel.load_model(ModelsaveConfig.model_store_file_path)
+# x = model.predict(None,('AFGDRVPCP742YM5MMLFIKZCGNNRQ',6))
+# print(x)
+
+
+# from dotenv import load_dotenv
+# import os
+# # Load the .env file
+# load_dotenv()
+# # Access the tracking URI
+# tracking_uri = os.getenv("TRACKING_URI")
+
+# print("Tracking URI:", tracking_uri)

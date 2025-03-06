4e078aa9b3f3caadd239c260d9a87290171a0d2e
Ankur Kumar <kumarankur0006@gmail.com>
2025-03-06 17:46:42 +0530

    update the app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import base64
 page_bg_img = f"""
 <style>
 .stApp {{
-    background-image: url("https://static.vecteezy.com/system/resources/previews/035/701/136/non_2x/set-of-fashion-clothes-for-women-casual-garments-and-accessories-for-spring-and-summer-jacket-bags-shoes-trousers-dress-hats-flying-flat-illustrations-isolated-on-white-background-vector.jpg");
+    background-image: url("https://as2.ftcdn.net/jpg/03/66/66/93/1000_F_366669347_Po2FsQ8tV00ILhQU37sd88lQDhXHYgon.jpg");
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
@@ -25,16 +25,33 @@ import pandas as pd
 import pymongo
 from dotenv import load_dotenv
 import os
+
+# Load environment variables
 load_dotenv()
-DB_NAME = os.getenv('DB_NAME')
-COLLECTION_NAME = os.getenv('COLLECTION_NAME')
-CONNECTION_URL = os.getenv('MONGODB_URL')
-client = pymongo.MongoClient(CONNECTION_URL)
-data_base = client[DB_NAME]
-collection = data_base[COLLECTION_NAME]
-df1 = pd.DataFrame(list(collection.find()))
-df = df1.drop_duplicates(subset=['item_id','image'])[['item_id', 'image']]
-user_ids = df1['user_id'].unique().tolist()
+
+DB_NAME = os.getenv("DB_NAME")
+COLLECTION_NAME = os.getenv("COLLECTION_NAME")
+CONNECTION_URL = os.getenv("MONGODB_URL")
+
+@st.cache_resource
+def get_database():
+    """Establish a persistent database connection."""
+    client = pymongo.MongoClient(CONNECTION_URL)
+    data_base = client[DB_NAME]
+    return data_base
+
+@st.cache_resource
+def get_data():
+    """Load and preprocess data once."""
+    data_base = get_database()
+    collection = data_base[COLLECTION_NAME]
+    df1 = pd.DataFrame(list(collection.find()))
+    df = df1.drop_duplicates(subset=["item_id", "image"])[["item_id", "image"]]
+    user_ids = df1["user_id"].unique().tolist()
+    return df, user_ids
+
+# Load data once
+df, user_ids = get_data()
 #----------------------------------------------------Loading Model For App--------------------------------------------------------------------
 import mlflow
 import mlflow.pyfunc
@@ -61,15 +78,23 @@ def load_model():
 model = load_model()
 # #------------------------------------------------------Making App------------------------------------------------------------------------------
 from src.utils.show_recommendation import RecommendedItemsDisplay
-# Selecting a user ids
-st.title('Recommend Items for a User')
+
+st.markdown(
+    "<h3 style='text-align: centre; font-size: 32px;'> Welcome to Adversial Multimedia Recommendation System For Robust Visual Recommendation ! </h3>",
+    unsafe_allow_html=True
+)
+
+st.markdown(
+    "<h3 style='text-align: left; font-size: 25px;'> This model recommends you visually similar items that you purchased earlier.</h3>",
+    unsafe_allow_html=True
+)
+
+st.markdown(
+    "<h3 style='text-align: left; font-size: 24px;'>Recommend Items for a User</h3>",
+    unsafe_allow_html=True
+)
 Select_UserID = st.selectbox("Select UserID to whom you want to recommend",user_ids)
 USER_ID = Select_UserID
-# Making Prediction
-#rec_item = model.predict(None,(USER_ID,6))
-# # Displaying
-#display = RecommendedItemsDisplay(rec_item, df, USER_ID)
-# show Recommendation
 if st.button('Show Recommendation'):
     rec_item = model.predict((USER_ID,6))
     display = RecommendedItemsDisplay(rec_item, df, USER_ID)
